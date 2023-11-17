a429b2ee23f59c35cd398a5c9985f66356d9665e
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-17 19:27:31 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,20 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+document.addEventListener('DOMContentLoaded', function () {
+  const galleryList = document.querySelector('.gallery');
+  let galleryMarkup = '';
+
+  galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+});
