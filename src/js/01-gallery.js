2fb7bd868b653657dbc81840c8716e2c3e3c124d
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-17 21:05:37 +0200

    5

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,13 +10,23 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 document.addEventListener('DOMContentLoaded', function () {
-  const galleryList = document.querySelector('.gallery');
-  let galleryMarkup = '';
-
-  galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
-
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
+    const galleryList = document.querySelector('.gallery');
+    let galleryMarkup = '';
+  
+    galleryItems.forEach(({ preview, original, description }) => {
+      galleryMarkup += `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    });
+  
+    galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+  
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
   });
-});
