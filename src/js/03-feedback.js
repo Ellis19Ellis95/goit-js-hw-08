4dd7a6c6d5d7d567179e6e7cf7b410c2080c72b1
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-19 17:03:56 +0200

    74

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,7 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
-const warningBlock = document.querySelector('.warning'); // Блок попередження
+const warningBlock = document.querySelector('.warning'); 
 const STORAGE_KEY = 'feedback-form-state';
 
 const updateStorage = () => {
@@ -37,7 +37,7 @@ form.addEventListener('submit', (event) => {
   };
 
   if (!formData.email || !formData.message) {
-    warningBlock.style.display = 'block'; // Показуємо блок попередження
+    warningBlock.style.display = 'block'; 
     warningBlock.textContent = 'Please fill in all fields';
     return;
   }
@@ -47,5 +47,5 @@ form.addEventListener('submit', (event) => {
   localStorage.removeItem(STORAGE_KEY);
   emailInput.value = '';
   messageInput.value = '';
-  warningBlock.style.display = 'none'; // Приховуємо блок попередження
+  warningBlock.style.display = 'none'; 
 });
